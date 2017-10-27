ebd6b0e3d601c18c500e3528cc21c3e707e333bc
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-27 19:14:15 +0530

    Changes:-
      1. Completed Side Bar
      2. Started work on homepage
      3. Added static elements

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import ScrollToTop from './lib/ScrollToTop';
 import Example from './containers/Example';
 import SideNavBar from './containers/SideNavBar';
+import Home from './containers/Home';
 
 const App = () => (
-  <div className="row">
-    <SideNavBar />
-    <BrowserRouter>
+  <BrowserRouter>
+    <div className="row">
+      <SideNavBar />
       <ScrollToTop>
         <main className="col-md-10">
-          <Route path="/" component={Example} />
+          <Route path="/" component={Home} />
           <Route path="/web" component={Example} />
         </main>
       </ScrollToTop>
-    </BrowserRouter>
-  </div>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
--- src/containers/SideNavBar.jsx
+++ src/containers/SideNavBar.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const SideNavBar = () => (
-  <sidebar className="col-md-2 side-nav-bar">
-    <div className="side-nav-content">
-      a
+  <div className="col-md-2 side-nav-bar">
+    <div className="text-center col-md-2 side-nav-content">
+      <header className="photo">
+        <img alt="avatar" src="/static/img/me.jpg" />
+        <h5>Yasser Hussain</h5>
+      </header>
+
+      <section className="nav">
+        <ul className="link-list">
+          <li><NavLink to="/">HOME</NavLink></li>
+          <li><NavLink to="/potfolio">PORTFOLIO</NavLink></li>
+          <li><NavLink to="/contact">CONTACT</NavLink></li>
+        </ul>
+      </section>
+
+      <footer>
+        <div className="social-links">
+          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+            <i className="fa fa-github" />
+          </a>
+
+          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+            <i className="fa fa-codepen" />
+          </a>
+
+          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+            <i className="fa fa-linkedin" />
+          </a>
+
+          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+            <i className="fa fa-skype" />
+          </a>
+
+          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+            <i className="fa fa-twitter" />
+          </a>
+        </div>
+
+        <div className="copyright">
+          <p>Copyright Yasser Hussain</p>
+          <p>@ 2017</p>
+        </div>
+      </footer>
     </div>
-  </sidebar>
+  </div>
 );
 
 export default SideNavBar;
