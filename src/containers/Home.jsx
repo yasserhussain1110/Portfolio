2b9767d9777dd40e1cf23f06cf4e9113d3191144
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-31 20:14:26 +0530

    Updated home page

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const Home = () => (
   <div className="home">
     <div className="bg-img-holder">
       <div className="info text-center">
         <h6>Yasser Hussain</h6>
-        <h1>Front End Coder</h1>
-        <p>Choose a job you love, and you will never have to work a day in your life.</p>
+        <h1>Programmer</h1>
+        <span>
+          &quot;Choose a job you love, and you will never
+          have to work a day in your life.&quot;
+        </span>
+        <br />
+        <i>― Confucius</i>
       </div>
     </div>
 
@@ -72,17 +78,45 @@ const Home = () => (
           <div className="col-xs">
             <i title="Git" className="devicon-git-plain" />
           </div>
+          <div className="col-xs">
+            <i title="Linux" className="devicon-linux-plain" />
+          </div>
         </div>
       </div>
 
 
       <div className="col-md-9 text-holder">
         <p>
-          I worked as a Java developer for 2 years when I came across NodeJS.
-          I was blown away by it and started learning it. I acquired fair
-          amount of competence in the Node stack by making a few hobby projects
-          and one full stack web app for an NGO.
-          Technologies I have worked with are given to the right.
+          I am a software developer based in Bangalore, India. I worked as Java/MySQL developer
+          for a few years when a friend of mine introduced me to&nbsp;
+          <span className="special">Node</span>. I was quickly blown away by it and started to
+          learn <span className="special">Node</span> and frontend technologies surrounding
+          it (like <span className="special">React</span>, <span className="special">Redux</span>,
+          <span className="special">Vue</span>) and Data Visualization Technologies
+          (like <span className="special">D3</span>).
+        </p>
+
+        <p>
+          Since the Node is mostly used with MongoDB, I started to learn MongoDB as well.
+          This is the first NoSQL database I came across, and it was totally different from
+          what I had seen before namely MySQL. This makes MongoDB very flexible and I totally
+          MongoDB for that.
+        </p>
+
+        <p>
+          After learning about the NodeJS ecosystem I started to build a few projects with it.
+          You can see my projects here&nbsp;
+          <NavLink to="/portfolio"><span className="special">Portfolio </span></NavLink>.
+          Then finally I built a full stack <span className="special">Web Application</span> for an
+          NGO to consolidate what I had learned. You can see it here&nbsp;
+          <a href="http://www.sonasarovartrust.org">
+            <span className="special">SonaSarovarTrust</span>
+          </a>.
+        </p>
+
+        <p>
+          I am looking for opportunities as Backend or a Frontend engineer on interesting
+          projects.
         </p>
       </div>
     </div>
@@ -91,17 +125,51 @@ const Home = () => (
     <div className="more-info-holder row middle-xs">
       <div className="col-md-3 icon-holder">
         <h6>PROVIDING VARIOUS SERVICES</h6>
-        <h2>Technologies I USE</h2>
+        <h2>Tech I have worked with</h2>
       </div>
 
 
       <div className="col-md-9 text-holder">
         <p>
-          I prefer using NODEJS for backend dev but I have professional experience
-          with Java and I have also used Phoenix/Elixir.
+          As I mentioned above I have experience in working with Java/MySQL, also I have done
+          web scraping with <span className="special">Perl</span>&nbsp;
+          (and <span className="special">Mechanize</span>).
+        </p>
+        <p>
+          Other <span className="special">Javascript</span> library I have worked with is&nbsp;
+          <span className="special">JQuery</span>.
+        </p>
+        <p>
+          Other tech related with <span className="special">Node</span>&nbsp;
+          that I have experience working with&nbsp;
+          <span className="special">Webpack</span>,&nbsp;
+          <span className="special">Socket.IO</span>,&nbsp;
+          <span className="special">Mocha</span>.
+        </p>
+      </div>
+    </div>
 
-          For frontend I mostly use React, Redux though I have made projects with
-          Vue, Vuex, etc.
+    <div className="interest-info-holder row middle-xs">
+      <div className="col-md-3 icon-holder">
+        <h2>Technologies that excite me</h2>
+      </div>
+
+
+      <div className="col-md-9 text-holder">
+        <p>
+          I love functional programming. See some of my publication related with function
+          programming on&nbsp;
+          <a href="https://medium.com/@yasserhussain1110">
+            <span className="special">Medium</span>
+          </a>.
+          One of the main reasons why I liked <span className="special">React</span> and&nbsp;
+          <span className="special">Redux</span> so much is because they take a functional approach.
+        </p>
+        <p>
+          I have started learning a new functional programming language and framework&nbsp;
+          <span className="special">Elixir</span> & <span className="special">Phoenix</span>.&nbsp;
+          See some of my projects with them on my&nbsp;
+          <NavLink to="/portfolio"><span className="special">Portfolio </span></NavLink>.
         </p>
       </div>
     </div>
