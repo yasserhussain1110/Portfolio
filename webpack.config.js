29016d6a9b764e2e44f3e090f582a41710a5465b
Yasser Hussain <yasserhussain1110@gmail.com>
2017-11-09 02:04:33 +0530

    Added img-loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,13 @@ module.exports = merge(baseWebpackConfig, {
           fallback: 'style-loader',
           use: 'css-loader'
         })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader'
+        ]
       }
     ]
   },
