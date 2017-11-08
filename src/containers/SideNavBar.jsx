94b4369105ca14a66c75bd10ead6511fbbd705fb
Yasser Hussain <yasserhussain1110@gmail.com>
2017-11-08 16:50:25 +0530

    Updated social links

--- src/containers/SideNavBar.jsx
+++ src/containers/SideNavBar.jsx
@@ -19,23 +19,19 @@ const SideNavBar = () => (
 
       <footer>
         <div className="social-links">
-          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/yasserhussain1110">
             <i className="fa fa-github" />
           </a>
 
-          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+          <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/yasserhussain1110">
             <i className="fa fa-codepen" />
           </a>
 
-          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/yasser-hussain-41001b45">
             <i className="fa fa-linkedin" />
           </a>
 
-          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
-            <i className="fa fa-skype" />
-          </a>
-
-          <a target="_blank" rel="noopener noreferrer" href="http://www.google.co.in">
+          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/yasserhussain11">
             <i className="fa fa-twitter" />
           </a>
         </div>
